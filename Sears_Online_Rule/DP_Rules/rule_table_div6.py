7ad0840f1b7078c162fc3624c564ee8128162502
jxiao0 <jianwei.xiao@searshc.com>
2018-07-10 03:52:49 -0500

    wrap pre, core, uplift, post into one function

--- Sears_Online_Rule/DP_Rules/rule_table_div6.py
+++ Sears_Online_Rule/DP_Rules/rule_table_div6.py
@@ -20,7 +20,7 @@ class DP_Rule_div6(dp_rules.DP_Rule_Constructor):
             func_desc='Table Selection')
 
     def get_pre_rule(self):
-        pre_rule_list = [
+        return [
             pre_rule.ad_plan_check,
             pre_rule.dp_block,
             pre_rule.clearance_check,
@@ -28,27 +28,25 @@ class DP_Rule_div6(dp_rules.DP_Rule_Constructor):
             pre_rule.min_margin_check,
             pre_rule.reg_check
         ]
-        return self.thisrule.pre_rule_wrapper(pre_rule_list)
+
 
     def get_core_rule(self):
-        core_rule_list = [
+        return [
             core_rule.Match_to_Min_comp_MM,
             core_rule.Match_to_Min_margin_when_Min_comp_Exists
         ]
-        return self.thisrule.core_rule_wrapper(core_rule_list)
+
 
     def get_uplift_rule(self):
-        uplift_rule_list =[
+        return [
             uplift_rule.uplift_by_uplift_table
         ]
-        return self.thisrule.uplift_wrapper(uplift_rule_list)
 
     def get_post_rule(self):
-        post_rule_list = [
+        return [
             post_rule.VD_Min_Reg_PMI_Upliftted_Prcie,
             post_rule.Min_Reg_PMI_Upliftted_Prcie_D_flag,
             post_rule.Reg_Bound_check_Null_when_reg_not_Exists
         ]
-        return self.thisrule.post_rule_wrapper(post_rule_list)
 
 
