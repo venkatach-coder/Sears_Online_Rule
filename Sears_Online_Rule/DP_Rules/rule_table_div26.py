c37b86b43f8dc33487410d60fd33e6ac4b40c0dd
YimingLi1994 <37124284+YimingLi1994@users.noreply.github.com>
2018-09-23 15:52:47 +0000

    HA brands 0.06 uplift (#46)
    * apparel uplift 10% 0921
    
    * HA brands uplift 0.06 0923
    
    * HA brands uplift 0.06 0923

--- Sears_Online_Rule/DP_Rules/rule_table_div26.py
+++ Sears_Online_Rule/DP_Rules/rule_table_div26.py
@@ -67,9 +67,9 @@ class Construct_DP_Rule(harlem.DP_Rule_Constructor):
         ]
 
     def get_uplift_rule(self):
+        func_handle = partial(uplift_rule._uplift_by_percentage_max, uplift=1.06, max_val = float('inf'))
         return [
-            # uplift_rule.uplift_those_with_subsidy,
-            # uplift_rule.uplift_by_uplift_table
+            Working_func(func_handle, '1.06 HA brands')
         ]
 
     def get_post_rule(self):
