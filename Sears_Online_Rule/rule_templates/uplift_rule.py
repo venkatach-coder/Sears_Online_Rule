26332e585e630f28cde8284270a2480056d87dca
YimingLi1994 <37124284+YimingLi1994@users.noreply.github.com>
2018-09-23 21:41:56 +0000

    change uplift rule name and turn off ee 0923 (#47)
    * apparel uplift 10% 0921
    
    * HA brands uplift 0.06 0923
    
    * HA brands uplift 0.06 0923
    
    * HA brands uplift naming change 0923
    
    * HA brands uplift naming change 0923

--- Sears_Online_Rule/rule_templates/uplift_rule.py
+++ Sears_Online_Rule/rule_templates/uplift_rule.py
@@ -36,7 +36,7 @@ def _PMI_uplift_with_max(row, uplift, max_val):  # Will not touch anything 99,99
 
 def _uplift_by_percentage_max(row, uplift, max_val):
     return round(min((uplift * row['core_rule_value']), row['core_rule_value'] + max_val), 2), \
-               'PMI uplift:{:.1f}%'.format((uplift - 1.0) * 100.0) + '' if math.isinf(max_val) \
+               'uplift:{:.1f}%'.format((uplift - 1.0) * 100.0) + '' if math.isinf(max_val) \
                    else ' max {:.2f}'.format(round(max_val, 2))
 
 def _uplift_those_with_subsidy(row):
