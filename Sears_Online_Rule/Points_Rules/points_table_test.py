80d26f7f71723571a65021c100eeb239adb7ce45
jxiao0 <jianwei.xiao2@searshc.com>
2018-11-07 13:25:07 -0600

    add points test rule (#106)

--- Sears_Online_Rule/Points_Rules/points_table_test.py
+++ Sears_Online_Rule/Points_Rules/points_table_test.py
@@ -6,7 +6,7 @@ from Sears_Online_Rule.harlem125_points_interface import Working_func_ext
 class Construct_DP_Rule(harlem.DP_Points_Rule_Constructor):
     def __init__(self):
         super().__init__(rule_level=1000,
-                         scope='div_no = 8 and itm_no in (1153, 1159, 1169, 1177, 1210, 1213, 1231, 1241, 1271, 1281)',
+                         scope='div_no = 8 and sales_90 < 0.01 ',
                          rule_name='points test')
 
     def get_merge_func(self):
@@ -22,10 +22,10 @@ class Construct_DP_Rule(harlem.DP_Points_Rule_Constructor):
 
     def get_core_rule(self):
         def points_10(row):
-            return int(10), 'points test, 10 points'
+            return random.randint(1,10), '1-10 points randomizer'
 
         return [
-             Working_func_ext(points_10, 'points test, 10 points')
+             Working_func_ext(points_10, '1-10 points randomizer')
         ]
 
     def get_post_rule(self):
