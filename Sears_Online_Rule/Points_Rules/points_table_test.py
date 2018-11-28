3b1c616a58454bea2cbfe9f2f94bee33b455c837
YimingLi1994 <37124284+YimingLi1994@users.noreply.github.com>
2018-11-28 19:31:04 +0000

    Stoppointstest1128 (#128)
    * thanksgiving remove uplift
    
    * stop points test 1128

--- Sears_Online_Rule/Points_Rules/points_table_test.py
+++ Sears_Online_Rule/Points_Rules/points_table_test.py
@@ -3,17 +3,20 @@ from Sears_Online_Rule.Points_Rules.points_templates import BUProgram
 import random
 from Sears_Online_Rule.harlem125_points_interface import Working_func_ext
 
+
 class Construct_DP_Rule(harlem.DP_Points_Rule_Constructor):
     def __init__(self):
         super().__init__(rule_level=1000,
                          scope='div_no = 8 and sales_90 < 0.01 and isdispelig is not null and reg < 25',
+                         is_active=False,
                          rule_name='points test')
 
     def get_merge_func(self):
         def merge_func(df_dict, scope):
             df1 = df_dict['full_rule_table_collision'].filter(scope) \
-                    .join(df_dict['VBS_hierarchy'], on =['div_no'], how='left')
+                .join(df_dict['VBS_hierarchy'], on=['div_no'], how='left')
             return df1
+
         return merge_func
 
     def get_pre_rule(self):
@@ -22,10 +25,10 @@ class Construct_DP_Rule(harlem.DP_Points_Rule_Constructor):
 
     def get_core_rule(self):
         def points_10(row):
-            return random.sample([5,10],1)[0], '5, 10 points randomizer'
+            return random.sample([5, 10], 1)[0], '5, 10 points randomizer'
 
         return [
-             Working_func_ext(points_10, '5, 10 points randomizer')
+            Working_func_ext(points_10, '5, 10 points randomizer')
         ]
 
     def get_post_rule(self):
@@ -34,7 +37,8 @@ class Construct_DP_Rule(harlem.DP_Points_Rule_Constructor):
 
     def get_points_expire_rule(self):
         def expire_14d(row):
-            return 14, 'Points expire in '+str(14) + ' days'
+            return 14, 'Points expire in ' + str(14) + ' days'
+
         return [
             Working_func_ext(expire_14d, 'points test, 14d expire')
         ]
