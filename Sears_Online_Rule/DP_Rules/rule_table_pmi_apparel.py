c21c506ffb6f56d8c047e1a24a536539876a44dc
jxiao0 <jianwei.xiao2@searshc.com>
2018-12-19 23:45:08 -0600

    remove apparel uplift (#150)

--- Sears_Online_Rule/DP_Rules/rule_table_pmi_apparel.py
+++ Sears_Online_Rule/DP_Rules/rule_table_pmi_apparel.py
@@ -49,10 +49,9 @@ class Construct_DP_Rule(harlem.DP_Rule_Constructor):
         ]
 
     def get_uplift_rule(self):
-        func_handle = partial(uplift_rule._uplift_by_percentage_max_no_free_shipping, uplift=1.08,
-                              min_val=1.0, max_val=5.0)
+        func_handle = partial(uplift_rule._uplift_by_percentage_max_no_free_shipping, uplift=1.00)
         return [
-            Working_func(func_handle, '1.08 Uplift min 1 max 5')
+            Working_func(func_handle, 'PMI')
         ]
         # return []
 
