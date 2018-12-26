623c3d00ffabc1b7b0b87c326caba9cc2652149e
jxiao0 <jianwei.xiao2@searshc.com>
2018-12-26 10:48:55 -0600

    remove all uplift, push 1.00 PMI (#153)

--- Sears_Online_Rule/DP_Rules/rule_table_div95.py
+++ Sears_Online_Rule/DP_Rules/rule_table_div95.py
@@ -51,7 +51,7 @@ class Construct_DP_Rule(harlem.DP_Rule_Constructor):
     def get_uplift_rule(self):
         func_handle = partial(uplift_rule._uplift_by_percentage_max_no_free_shipping, uplift=1.12, max_val=float('inf'))
         return [
-            Working_func(func_handle, '1.12 Uplift')
+            #Working_func(func_handle, '1.12 Uplift')
         ]
 
     def get_post_rule(self):
