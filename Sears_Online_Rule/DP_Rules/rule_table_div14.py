3ceceb55c6e3345424c2894dacc53c127ed8d741
jxiao0 <jianwei.xiao2@searshc.com>
2019-05-04 13:36:42 -0500

    Uplift 12.5% for div8 and 6% for all others (#180)

--- Sears_Online_Rule/DP_Rules/rule_table_div14.py
+++ Sears_Online_Rule/DP_Rules/rule_table_div14.py
@@ -65,9 +65,9 @@ class Construct_DP_Rule(harlem.DP_Rule_Constructor):
         ]
 
     def get_uplift_rule(self):
-        func_handle = partial(uplift_rule._uplift_by_percentage_max_no_free_shipping, uplift=1.07, max_val=5.0)
+        func_handle = partial(uplift_rule._uplift_by_percentage_max, uplift=1.06)
         return [
-            Working_func(func_handle, '1.07 Uplift Max $5')
+            Working_func(func_handle, '1.06 Uplift')
         ]
 
     def get_post_rule(self):
--- Sears_Online_Rule/DP_Rules/rule_table_div8.py
+++ Sears_Online_Rule/DP_Rules/rule_table_div8.py
@@ -70,9 +70,9 @@ class Construct_DP_Rule(harlem.DP_Rule_Constructor):
         ]
 
     def get_uplift_rule(self):
-        func_handle = partial(uplift_rule._uplift_by_percentage_max_no_free_shipping, uplift=1.07, max_val=5.0)
+        func_handle = partial(uplift_rule._uplift_by_percentage_max, uplift=1.125)
         return [
-            Working_func(func_handle, '1.07 Uplift Max $5')
+            Working_func(func_handle, '12.5% Uplift')
         ]
 
     def get_post_rule(self):
