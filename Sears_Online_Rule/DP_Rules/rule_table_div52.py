3909ea932bc366ce20e66cb06da6af794a4a929e
jxiao0 <jianwei.xiao2@searshc.com>
2019-11-13 23:50:33 -0600

    toy vd delete (#200)

--- Sears_Online_Rule/DP_Rules/rule_table_div52.py
+++ Sears_Online_Rule/DP_Rules/rule_table_div52.py
@@ -62,6 +62,7 @@ class Construct_DP_Rule(harlem.DP_Rule_Constructor):
         return [
             core_rule.Match_to_Min_comp_MM,
             core_rule.Match_to_Min_margin_when_Min_comp_Exists,
+            core_rule.VD_delete
         ]
 
 
